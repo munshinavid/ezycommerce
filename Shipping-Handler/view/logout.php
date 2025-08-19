bb1eddc13155afb4066b841665308020678f2841
Navid <navid123@gmail.com>
2025-08-19 19:21:38 +0600

    Refactor project roles: remove old admin/shipping/manager folders and add new role-based structure

--- Shipping-Handler/view/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-setcookie(session_name(), '', time() - 3600, '/'); // Delete session cookie
-header("Location: login.php");
-exit();
-?>
