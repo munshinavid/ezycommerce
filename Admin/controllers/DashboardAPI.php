d07d296c31b1f0605df57c71da73c93da141116a
munshinavid <navidmunshi24@gmail.com>
2026-05-25 03:37:59 +0600

    CI/CD Test

--- Admin/controllers/DashboardAPI.php
+++ Admin/controllers/DashboardAPI.php
@@ -4,6 +4,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
+// ci/cd test
+
 require_once __DIR__ . '/../models/Database.php';
 require_once __DIR__ . '/../../utils/UrlHelper.php';
 
